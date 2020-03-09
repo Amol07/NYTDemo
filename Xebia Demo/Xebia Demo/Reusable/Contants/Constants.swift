6a130e0690859d562a62b2e9f1e86ad91494ece0
Amol Prakash <amolprakash06@gmail.com>
2020-03-10 04:33:28 +0530

    Added constants, service, downloader and error handling to the project

---
+++ Xebia Demo/Xebia Demo/Reusable/Contants/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  Xebia Demo
+//
+//  Created by Amol Prakash on 09/03/20.
+//  Copyright © 2020 Amol Prakash. All rights reserved.
+//
+
+import Foundation
+
+/*
+ * Use this struct to group all of the constants in the app.
+ */
+struct Constant {
+    struct APIConstant {
+        static let defaultPageIndex: UInt = 0
+        static let offset: UInt = 20
+        static let apiKey = "8O3iaLobQ3Tzs5fpqhtBIZAY9UCWVpgW"
+        static let scheme = "https"
+        static let host = "api.nytimes.com"
+        static let viewedPath = "viewed"
+    }
+}
---
+++ Xebia Demo/Xebia Demo/Reusable/Service/EndPoints.swift
@@ -0,0 +1,51 @@
+//
+//  EndPoints.swift
+//  Xebia Demo
+//
+//  Created by Amol Prakash on 09/03/20.
+//  Copyright © 2020 Amol Prakash. All rights reserved.
+//
+
+import Foundation
+
+enum EndPoints {
+    case getContents(pageIndex: UInt, offset: UInt, path: String)
+    
+    var method: HTTPMethod {
+        switch self {
+        case .getContents:
+            return .get
+        }
+    }
+    
+    var url: URL {
+        var component = URLComponents()
+        component.scheme = Constant.APIConstant.scheme
+        component.host = Constant.APIConstant.host
+        component.path = self.path
+        component.queryItems = self.queryItems + [URLQueryItem(name: "api-key", value: Constant.APIConstant.apiKey)]
+        return component.url! //Force unwrapping to make sure URL never nil
+    }
+}
+
+// MARK: - Privates
+
+extension EndPoints {
+    fileprivate var path: String {
+        switch self {
+        case .getContents(_, _, let path):
+            return "/svc/mostpopular/v2/\(path))/30.json"
+        }
+    }
+    
+    fileprivate var queryItems: [URLQueryItem] {
+        switch self {
+        case .getContents(let pageIndex, let offset, _):
+            return [URLQueryItem(name: "offset", value: "\(pageIndex * offset)")]
+        }
+    }
+}
+
+enum HTTPMethod: String {
+    case get = "GET"
+}
